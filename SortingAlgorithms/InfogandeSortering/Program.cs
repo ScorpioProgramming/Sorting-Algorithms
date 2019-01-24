3add51352acaa497ff1277487493797821ffa181
Samuel Berg <ScorpioProgramming@users.noreply.github.com>
2019-01-24 07:34:28 +0100

    Small changes
    Added so you can see the time it took in nanoseconds, and made it so that you can scroll through all of the diffrent times it took for each algorithm at each difficulty.

--- SortingAlgorithms/InfogandeSortering/Program.cs
+++ SortingAlgorithms/InfogandeSortering/Program.cs
@@ -28,6 +28,9 @@ namespace InfogandeSortering
 
             Stopwatch sw = new Stopwatch();  //Stopwatch
 
+            double frequency = Stopwatch.Frequency; //Making it possible to see amount of naniseconds it takes
+            double nanosecPerTick = (1000 * 1000 * 1000) / frequency;
+
             double Atime = -1;   //Save location of times
             double Btime = -1;
             double Ctime = -1;
@@ -60,90 +63,106 @@ namespace InfogandeSortering
             }
 
             Console.WriteLine("Sorting algorithms timed tests");    //Writing out the type of sorting algorithm and the amount of elements inside of that array. First with 10 numbers in each array, then 1000 numbers and at last 100000 numbers.
-            Thread.Sleep(5000); //Wait 5 seconds
+            Thread.Sleep(2000); //Wait 2 seconds
             Console.ReadLine(); //User has to press enter to start the sorting. Has to do that for all of the other sorting parts aswell.
             Console.WriteLine("Insertionsort with 10 numbers");
             sw.Start();
-            //start sorting(Insertionsort)
+                //start sorting(Insertionsort)
             sw.Stop();
-            Atime = sw.Elapsed.TotalMilliseconds;
-            Console.WriteLine(sw.Elapsed.TotalMilliseconds + " ms");
+            Atime = sw.ElapsedTicks * nanosecPerTick;
+            Console.WriteLine(sw.ElapsedTicks * nanosecPerTick + " ns");
             sw.Reset();
             Console.ReadLine();
 
             Console.WriteLine("Mergesort with 10 numbers");
             sw.Start();
-            //start sorting(Mergsort)
+                //start sorting(Mergsort)
             sw.Stop();
-            Dtime = sw.Elapsed.TotalMilliseconds;
-            Console.WriteLine(sw.Elapsed.TotalMilliseconds + " ms");
+            Dtime = sw.ElapsedTicks * nanosecPerTick;
+            Console.WriteLine(sw.ElapsedTicks * nanosecPerTick + " ns");
             sw.Reset();
             Console.ReadLine();
 
             Console.WriteLine("Quicksort with 10 numbers");
             sw.Start();
-            //start sorting(Quicksort)
+                //start sorting(Quicksort)
             sw.Stop();
-            Gtime = sw.Elapsed.TotalMilliseconds;
-            Console.WriteLine(sw.Elapsed.TotalMilliseconds + " ms");
+            Gtime = sw.ElapsedTicks * nanosecPerTick;
+            Console.WriteLine(sw.ElapsedTicks * nanosecPerTick + " ns");
             sw.Reset();
             Console.ReadLine();
 
             Console.WriteLine("Insertionsort with 1000 numbers");
             sw.Start();
-            //start sorting(Insertionsort)
+                //start sorting(Insertionsort)
             sw.Stop();
-            Btime = sw.Elapsed.TotalMilliseconds;
-            Console.WriteLine(sw.Elapsed.TotalMilliseconds + " ms");
+            Btime = sw.ElapsedTicks * nanosecPerTick;
+            Console.WriteLine(sw.ElapsedTicks * nanosecPerTick + " ns");
             sw.Reset();
             Console.ReadLine();
 
             Console.WriteLine("Mergesort with 1000 numbers");
             sw.Start();
-            //start sorting(Mergsort)
+                //start sorting(Mergsort)
             sw.Stop();
-            Etime = sw.Elapsed.TotalMilliseconds;
-            Console.WriteLine(sw.Elapsed.TotalMilliseconds + " ms");
+            Etime = sw.ElapsedTicks * nanosecPerTick;
+            Console.WriteLine(sw.ElapsedTicks * nanosecPerTick + " ns");
             sw.Reset();
             Console.ReadLine();
 
             Console.WriteLine("Quicksort with 1000 numbers");
             sw.Start();
-            //start sorting(Quicksort)
+                //start sorting(Quicksort)
             sw.Stop();
-            Htime = sw.Elapsed.TotalMilliseconds;
-            Console.WriteLine(sw.Elapsed.TotalMilliseconds + " ms");
+            Htime = sw.ElapsedTicks * nanosecPerTick;
+            Console.WriteLine(sw.ElapsedTicks * nanosecPerTick + " ns");
             sw.Reset();
             Console.ReadLine();
 
             Console.WriteLine("Insertionsort with 100000 numbers");
             sw.Start();
-            //start sorting(Insertionsort)
+                //start sorting(Insertionsort)
             sw.Stop();
-            Ctime = sw.Elapsed.TotalMilliseconds;
-            Console.WriteLine(sw.Elapsed.TotalMilliseconds + " ms");
+            Ctime = sw.ElapsedTicks * nanosecPerTick;
+            Console.WriteLine(sw.ElapsedTicks * nanosecPerTick + " ns");
             sw.Reset();
             Console.ReadLine();
 
             Console.WriteLine("Mergesort with 100000 numbers");
             sw.Start();
-            //start sorting(Mergsort)
+                //start sorting(Mergsort)
             sw.Stop();
-            Ftime = sw.Elapsed.TotalMilliseconds;
-            Console.WriteLine(sw.Elapsed.TotalMilliseconds + " ms");
+            Ftime = sw.ElapsedTicks * nanosecPerTick;
+            Console.WriteLine(sw.ElapsedTicks * nanosecPerTick + " ns");
             sw.Reset();
             Console.ReadLine();
 
             Console.WriteLine("Quicksort with 100000 numbers");
             sw.Start();
-            //start sorting(Quicksort)
+                //start sorting(Quicksort)
             sw.Stop();
-            Jtime = sw.Elapsed.TotalMilliseconds;
-            Console.WriteLine(sw.Elapsed.TotalMilliseconds + " ms");
+            Jtime = sw.ElapsedTicks * nanosecPerTick;
+            Console.WriteLine(sw.ElapsedTicks * nanosecPerTick + " ns");
             sw.Reset();
             Console.ReadLine();
 
             //Write out all of the times categorized by type of sorting. Strating from Insertonsort to Quicksort.
+            Console.WriteLine("Insertionsort:");
+            Console.WriteLine("Array with 10 elements took: " + Atime + " ns");
+            Console.WriteLine("Array with 1000 elements took: " + Btime + " ns");
+            Console.WriteLine("Array with 100000 elements took: " + Ctime + " ns");
+
+            Console.WriteLine("Mergsort:");
+            Console.WriteLine("Array with 10 elements took: " + Dtime + " ns");
+            Console.WriteLine("Array with 1000 elements took: " + Etime + " ns");
+            Console.WriteLine("Array with 100000 elements took: " + Ftime + " ns");
+
+            Console.WriteLine("Quicksort:");
+            Console.WriteLine("Array with 10 elements took: " + Gtime + " ns");
+            Console.WriteLine("Array with 1000 elements took: " + Htime + " ns");
+            Console.WriteLine("Array with 100000 elements took: " + Jtime + " ns");
+
+            Console.ReadLine(); //So that we can look through all of the diffrent times, and see what algorithm were the fastest.
         }
     }
 }
